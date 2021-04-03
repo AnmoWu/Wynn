5b03d2e42f01f65085b6f0e75e9432bcb984fd53
Anmo Wu <X3.141592653@outlook.com>
2021-04-03 17:18:54 +0800

    Slow - 落日飞车

---
+++ Train23/unity.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<assert.h>
+//#include<vector>
+//#include<time.h>
+using namespace std;
+#define LAST 1000
+const double pi = 4.0 * atan(1.0);
+
+//char buf[LAST];
+//int p[LAST];
+
+
+int n;
+char word[2000][10], sorted[2000][10];
+
+int cmp_char(const void* _a, const void* _b);
+int cmp_string(const void* _a, const void* _b);
+
+int main()
+{
+    int n = 0;
+    for (;;)
+    {
+        scanf("%s", word[n]);
+        if (word[n][0] == '*')break;
+        n++;
+    }
+    qsort(word, n, sizeof(word[0]), cmp_string);
+    for (int i = 0; i < n; i++)
+    {
+        strcpy(sorted[i], word[i]);
+        qsort(sorted[i], strlen(sorted[i]), sizeof(char), cmp_char);
+    }
+    char s[10];
+    while (scanf("%s", s) == 0)
+    {
+        qsort(s, strlen(s), sizeof(char), cmp_char);
+        int found = 0;
+        for(int i=0;i<n;i++)
+            if (strcmp(sorted[i], s) == 0)
+            {
+                found = 1;
+                printf("%s", word[i]);
+            }
+        if (!found)printf(":(");
+        printf("\n");
+    }
+    return 0;
+}
+
+int cmp_char(const void* _a, const void* _b)
+{
+    char* a = (char*)_a;
+    char* b = (char*)_b;
+    return *a - *b;
+}
+
+int cmp_string(const void* _a, const void* _b)
+{
+    char* a = (char*)_a;
+    char* b = (char*)_b;
+    return strcmp(a, b);
+}
